a02fd0b9dc3ac681ec95783d39fdfe69205b40fd
Manoj <manoj.khanwalkar@gmail.com>
2015-08-08 13:44:58 -0400

    added model objects

--- src/main/java/oms/OMS.java
+++ src/main/java/oms/OMS.java
@@ -1,6 +1,20 @@
 package oms;
 
+import cache.Cache;
+import model.Detail;
+import model.Execution;
+import model.Order;
+import model.Transaction;
+
+import java.util.List;
+
 public class OMS {
+
+    Cache<String, Order> orderCache = new Cache<>();
+    Cache<String, Execution> executionCache = new Cache<>();
+    Cache<String, Detail> detailCache = new Cache<>();
+    Cache<String, Transaction> transactionCache = new Cache<>();
+    Cache<String, List> listCache = new Cache<>();
 
 
     static class Holder {
