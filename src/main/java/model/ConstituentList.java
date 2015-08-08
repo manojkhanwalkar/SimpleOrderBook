83645510ebf20a9191afa502de50ebd7fe106dfc
Manoj <manoj.khanwalkar@gmail.com>
2015-08-08 12:39:46 -0400

    added model objects

--- src/main/java/model/ConstituentList.java
+++ src/main/java/model/ConstituentList.java
@@ -5,5 +5,19 @@ public class ConstituentList {
     String listId;
     int percentage ;
 
+    public String getListId() {
+        return listId;
+    }
 
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
 }
--- src/main/java/model/Detail.java
+++ src/main/java/model/Detail.java
@@ -4,4 +4,19 @@ public class Detail {
     String orderId;
     String account ;
 
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
 }
